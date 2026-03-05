90819af872df7dfb417069dcef0a4de2d3b3e37f
rockybanna <ajayrajpoot420@gmail.com>
2026-03-05 14:48:03 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,6 @@
-const supabase = window.supabase.createClient(
+// Create a Supabase client using the CDN library loaded in index.html
+
+const client = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
